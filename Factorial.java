3b843f61e599e68c82c16d76d7157e01ed71698f
Prasad2083 <Varavam2083@gmail.com>
2020-09-13 11:34:46 -0400

    Revert "Modified Factorial Program"
    This reverts commit b7502fa8e19fe3322ec7e71ad6727fc4452cdb3b.

--- Factorial.java
+++ Factorial.java
@@ -2,13 +2,9 @@ import java.util.Scanner;
 
 public class Factorial {
     public static void main(String[] args) {
-	    // Implement scanner class to give user input
-	    
-
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a number to get factorial of it");
         int num=sc.nextInt();
-	System.out.println("Factorial of a number")
         int fact=1;
         for (int i=1;i<=num;i++){
             fact=fact*i;
